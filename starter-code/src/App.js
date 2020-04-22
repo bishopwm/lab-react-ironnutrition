e51e02bb288f69a0891e2328b6177bfdde1cd959
bishopwm <bishopwm@g.cofc.edu>
2020-04-22 11:35:44 -0400

    fixed list format

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -14,10 +14,39 @@ class App extends Component {
       foodsList.push(
         <li key={i} className="food-list-item">
           <div>
-            <img className="food-image"src={this.state.allFoods[i].image} alt=""></img><br></br>
-            <strong>{this.state.allFoods[i].name}</strong><br></br>
-            <strong>Cal: </strong>{this.state.allFoods[i].calories}<br></br>
-            <strong>Quant: </strong>{this.state.allFoods[i].quantity}
+            <div className="box">
+              <article className="media">
+                <div className="media-left">
+                  <figure className="image is-64x64">
+                  <img style={{width: '6%'}} src={this.state.allFoods[i].image} alt=""/>
+                  </figure>
+                </div>
+                <div className="media-content">
+                  <div className="content">
+                    <p>
+                      <strong>{this.state.allFoods[i].name}</strong> <br />
+                      <small>{this.state.allFoods[i].calories}</small>
+                    </p>
+                  </div>
+                </div>
+                <div className="media-right">
+                  <div className="field has-addons">
+                    <div className="control">
+                      <input
+                        className="input"
+                        type="number" 
+                        value={this.state.allFoods[i].quantity}
+                      />
+                    </div>
+                    <div className="control">
+                      <button className="button is-info">
+                        +
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              </article>
+            </div>
           </div>
         </li>
       )}
@@ -30,42 +59,15 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Iron Nutrition</h1>
+        <form>
+          <input placeholder="Food Name" type="text"></input><br></br>
+          <input placeholder="Food Calories" type="number"></input><br></br>
+          <input placeholder="Food Image" type="url"></input><br></br>
+        </form>
+        <button onClick={this.addFood}>Add Food</button>
         <div>
         {this.displayAllFoods()}
-        </div>
-        <div className="box">
-          <article className="media">
-            <div className="media-left">
-              <figure className="image is-64x64">
-              <img style={{width: '6%'}} src="https://i.imgur.com/eTmWoAN.png" alt=""/>
-              </figure>
-            </div>
-            <div className="media-content">
-              <div className="content">
-                <p>
-                  <strong>Pizza</strong> <br />
-                  <small>400 cal</small>
-                </p>
-              </div>
-            </div>
-            <div className="media-right">
-              <div className="field has-addons">
-                <div className="control">
-                  <input
-                    className="input"
-                    type="number" 
-                    value="1"
-                  />
-                </div>
-                <div className="control">
-                  <button className="button is-info">
-                    +
-                  </button>
-                </div>
-              </div>
-            </div>
-          </article>
-        </div>
+      </div>
       </div>
     );
   }
